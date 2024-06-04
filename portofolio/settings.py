c79acd338f54e1d17eaaa9949374ca2d36bac2a8
Dennis Kinanga <denniskinanga6@gmail.com>
2024-06-04 13:53:34 +0300

    deploy

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
 
 
 
@@ -91,13 +92,17 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+    "default":dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
